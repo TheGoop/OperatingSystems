bb55b917113a42441cda51a398d4658ce4673ba9
Jon Eyolfson <jon@eyl.io>
2021-03-26 21:14:10 -0400

    Added Lab 0 skeleton

---
+++ lab-00/proc_count.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/printk.h>
+
+static int __init proc_count_init(void)
+{
+	pr_info("proc_count: init\n");
+	return 0;
+}
+
+static void __exit proc_count_exit(void)
+{
+	pr_info("proc_count: exit\n");
+}
+
+module_init(proc_count_init);
+module_exit(proc_count_exit);
+
+MODULE_AUTHOR("Your name");
+MODULE_DESCRIPTION("One sentence description");
+MODULE_LICENSE("GPL");
