bc2b594a8a4f91c88995664925578f35013fd0d1
Akshay Gupta <akshaygupta9@ucla.edu>
2021-04-07 13:31:58 -0700

    added functionality to add and remove file and to count all processes

--- lab-00/proc_count.c
+++ lab-00/proc_count.c
@@ -1,20 +1,38 @@
 #include <linux/module.h>
 #include <linux/printk.h>
 
+static struct proc_dir_entry *count_file;
+static int proc_count_show(struct seq_file *m, void *v)
+{
+	struct task_struct *p;
+	int count = 0;
+	for_each_process(p)
+	{
+		count = count + 1;
+	}
+	
+	seq_printf(m, "%d\n", count); 
+	return 0;
+}
+
+
 static int __init proc_count_init(void)
 {
 	pr_info("proc_count: init\n");
+	count_file = proc_create_single("count", 0, NULL, proc_count_show);
+	
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
 	pr_info("proc_count: exit\n");
+	proc_remove(count_file);
 }
 
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your name");
-MODULE_DESCRIPTION("One sentence description");
+MODULE_AUTHOR("Akshay Gupta");
+MODULE_DESCRIPTION("This module reports the number of running processes on the machine.");
 MODULE_LICENSE("GPL");
