7dd7a0a80cf4a2a66f1436dc7bbf8fd678cffb26
Akshay Gupta <akshaygupta9@ucla.edu>
2021-04-16 20:52:17 -0700

    made some skeletons

--- lab-01/pipe.c
+++ lab-01/pipe.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
+
+char* run_process(char* process, char* input)
+{
+	//Executes and returns the stdout of the process that runs
+	
+	return "";
+}
+
 int main(int argc, char *argv[])
 {
+	/*
 	int i;
 	for (i = 1; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
+	*/
+	
+	//iterate through every process from left to right 1 -> argc
+	//execute each process with the stdin of the stdout of the prev process
+	//print to stdout the stoud of the last process to run
+
 	return 0;
 }
