d06aa534ae971a1f81117dd00a1073c603551b27
Akshay Gupta <akshaygupta.us@gmail.com>
2021-05-10 00:47:52 -0400

    need to test, pretty much everything has been completed

--- lab-02/rr.c
+++ lab-02/rr.c
@@ -12,38 +12,50 @@
 typedef uint32_t u32;
 typedef int32_t i32;
 
-struct process {
+struct process
+{
   u32 pid;
   u32 arrival_time;
   u32 burst_time;
 
-  TAILQ_ENTRY(process) pointers;
+  TAILQ_ENTRY(process)
+  pointers;
 
   /* Additional fields here */
+  bool has_started;
+  u32 remaining_time;
+  u32 rr_time;
   /* End of "Additional fields here" */
 };
 
 TAILQ_HEAD(process_list, process);
 
-u32 next_int(const char **data, const char *data_end) {
+u32 next_int(const char **data, const char *data_end)
+{
   u32 current = 0;
   bool started = false;
-  while (*data != data_end) {
+  while (*data != data_end)
+  {
     char c = **data;
 
-    if (c < 0x30 || c > 0x39) {
-      if (started) {
-	return current;
+    if (c < 0x30 || c > 0x39)
+    {
+      if (started)
+      {
+        return current;
       }
     }
-    else {
-      if (!started) {
-	current = (c - 0x30);
-	started = true;
+    else
+    {
+      if (!started)
+      {
+        current = (c - 0x30);
+        started = true;
       }
-      else {
-	current *= 10;
-	current += (c - 0x30);
+      else
+      {
+        current *= 10;
+        current += (c - 0x30);
       }
     }
 
@@ -54,20 +66,25 @@ u32 next_int(const char **data, const char *data_end) {
   exit(EINVAL);
 }
 
-u32 next_int_from_c_str(const char *data) {
+u32 next_int_from_c_str(const char *data)
+{
   char c;
   u32 i = 0;
   u32 current = 0;
   bool started = false;
-  while ((c = data[i++])) {
-    if (c < 0x30 || c > 0x39) {
+  while ((c = data[i++]))
+  {
+    if (c < 0x30 || c > 0x39)
+    {
       exit(EINVAL);
     }
-    if (!started) {
+    if (!started)
+    {
       current = (c - 0x30);
       started = true;
     }
-    else {
+    else
+    {
       current *= 10;
       current += (c - 0x30);
     }
@@ -80,14 +97,16 @@ void init_processes(const char *path,
                     u32 *process_size)
 {
   int fd = open(path, O_RDONLY);
-  if (fd == -1) {
+  if (fd == -1)
+  {
     int err = errno;
     perror("open");
     exit(err);
   }
 
   struct stat st;
-  if (fstat(fd, &st) == -1) {
+  if (fstat(fd, &st) == -1)
+  {
     int err = errno;
     perror("stat");
     exit(err);
@@ -95,7 +114,8 @@ void init_processes(const char *path,
 
   u32 size = st.st_size;
   const char *data_start = mmap(NULL, size, PROT_READ, MAP_PRIVATE, fd, 0);
-  if (data_start == MAP_FAILED) {
+  if (data_start == MAP_FAILED)
+  {
     int err = errno;
     perror("mmap");
     exit(err);
@@ -103,30 +123,32 @@ void init_processes(const char *path,
 
   const char *data_end = data_start + size;
   const char *data = data_start;
-  
 
   *process_size = next_int(&data, data_end);
 
   *process_data = calloc(sizeof(struct process), *process_size);
-  if (*process_data == NULL) {
+  if (*process_data == NULL)
+  {
     int err = errno;
     perror("calloc");
     exit(err);
   }
 
-  for (u32 i = 0; i < *process_size; ++i) {
+  for (u32 i = 0; i < *process_size; ++i)
+  {
     (*process_data)[i].pid = next_int(&data, data_end);
     (*process_data)[i].arrival_time = next_int(&data, data_end);
     (*process_data)[i].burst_time = next_int(&data, data_end);
   }
-  
+
   munmap((void *)data, size);
   close(fd);
 }
 
 int main(int argc, char *argv[])
 {
-  if (argc != 3) {
+  if (argc != 3)
+  {
     return EINVAL;
   }
   struct process *data;
@@ -135,17 +157,85 @@ int main(int argc, char *argv[])
 
   u32 quantum_length = next_int_from_c_str(argv[2]);
 
-  struct process_list list;
+  struct process_list list; //list of every process that is active/has to run
   TAILQ_INIT(&list);
 
   u32 total_waiting_time = 0;
   u32 total_response_time = 0;
 
   /* Your code here */
+  // set all of the processes' remaining times
+  struct process *p;
+  for (u32 i = 0; i < size; i++)
+  {
+    p = &data[i];
+    p->remaining_time = p->burst_time;
+    p->has_started = false;
+    p->rr_time = quantum_length;
+  }
+
+  u32 t = 0;
+  bool finished = false;
+  while (!finished)
+  {
+    //load any new processes into the list
+    for (u32 i = 0; i < size; i++)
+    {
+      p = &data[i];
+      if (p->arrival_time == t)
+      {
+        p->remaining_time = p->burst_time;
+        p->has_started = false;
+        p->rr_time = quantum_length;
+        TAILQ_INSERT_TAIL(&list, p, pointers);
+        //we insert at the tail, because newly recieved processes go to the end of the queue not front
+      }
+    }
+
+    if (!TAILQ_EMPTY(&list))
+    {
+      p = TAILQ_FIRST(&list);
+
+      // if this hasn't recieved a response yet, update the total response time
+      if (p->has_started == false)
+      {
+        total_response_time += t - p->arrival_time;
+      }
+
+      //"run" p
+      p->rr_time -= 1;
+      p->remaining_time -= 1;
+
+      // if p is done running
+      if (p->remaining_time <= 0)
+      {
+        total_waiting_time += t - (p->burst_time + p->arrival_time);
+        //remove from list of processes
+        TAILQ_REMOVE(&list, p, pointers);
+      }
+
+      // if p doesn't have time left in its rr turn
+      if (p->rr_time <= 0)
+      {
+        p->rr_time = quantum_length; //reset its turn timer
+        //move p to back of the queue
+        TAILQ_REMOVE(&list, p, pointers);
+        TAILQ_INSERT_TAIL(&list, p, pointers);
+      }
+    }
+
+    t += 1;
+
+    if (t > 900)
+    {
+      break;
+    }
+  }
+
   /* End of "Your code here" */
 
-  printf("Average waiting time: %.2f\n", (float) total_waiting_time / (float) size);
-  printf("Average response time: %.2f\n", (float) total_response_time / (float) size);
+  printf("Average waiting time: %.2f\n", (float)total_waiting_time / (float)size);
+  printf("Average response time: %.2f\n", (float)total_response_time / (float)size);
 
   free(data);
   return 0;
