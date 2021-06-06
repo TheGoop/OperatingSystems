13879bd7795920d06b7a27445cd5ec4945ad636f
Akshay Gupta <akshaygupta.us@gmail.com>
2021-06-06 16:35:55 -0400

    added all of the inode definitoins for write_inode_table

--- lab-04/ext2-create.c
+++ lab-04/ext2-create.c
@@ -414,7 +414,7 @@ void write_inode_table(int fd)
 	/* Hello sym link */
 	struct ext2_inode hello_inode = {0};
 	hello_inode.i_mode = EXT2_S_IFLNK | EXT2_S_IRUSR | EXT2_S_IWUSR | EXT2_S_IRGRP | EXT2_S_IROTH;
-	hello_inode.i_uid = 100;
+	hello_inode.i_uid = 1000;
 	hello_inode.i_size = 11; // since its a soft link and points to "hello-world" which is 11 bytes
 	hello_inode.i_atime = current_time;
 	hello_inode.i_ctime = current_time;
@@ -422,11 +422,24 @@ void write_inode_table(int fd)
 	hello_inode.i_dtime = 0;
 	hello_inode.i_gid = 1000;
 	hello_inode.i_links_count = 1;
-	hello_inode.i_blocks = 2;				  //not sure about this
-	hello_inode.i_block[0] = HELLO_WORLD_INO; //not sure about this
+	// hello_inode.i_blocks = 2;	//not sure about this
+	// hello_inode.i_block[0] = 0; //not sure about this
 	write_inode(fd, HELLO_INO, &hello_inode);
 
-	/* */
+	/* root directory inode */
+	struct ext2_inode root_inode = {0};
+	root_inode.i_mode = EXT2_S_IFDIR | EXT2_S_IRUSR | EXT2_S_IWUSR | EXT2_S_IXUSR | EXT2_S_IRGRP | EXT2_S_IXGRP | EXT2_S_IROTH | EXT2_S_IXOTH;
+	root_inode.i_uid = 0;
+	root_inode.i_size = 1024;
+	root_inode.i_atime = current_time;
+	root_inode.i_ctime = current_time;
+	root_inode.i_mtime = current_time;
+	root_inode.i_dtime = 0;
+	root_inode.i_gid = 0;
+	root_inode.i_links_count = 3;
+	root_inode.i_blocks = 2; //not sure about this
+	root_inode.i_block[0] = ROOT_DIR_BLOCKNO;
+	write_inode(fd, EXT2_ROOT_INO, &root_inode);
 }
 
 void write_root_dir_block(int fd)
