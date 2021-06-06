ae11aac71ddd015262d9a70bb187e1e30635e09a
Akshay Gupta <akshaygupta.us@gmail.com>
2021-06-06 19:15:00 -0400

    padding fix

--- lab-04/ext2-create.c
+++ lab-04/ext2-create.c
@@ -319,7 +319,7 @@ void write_block_bitmap(int fd)
 			block_bitmap[i] = 0xFF;
 		}
 	}
-	block_bitmap[127] = 0b1111110;
+	block_bitmap[127] = 0b1111111;
 
 	if (write(fd, block_bitmap, NUM_BLOCKS) == -1)
 	{
